b17afaa05af949d44ab06d2b9c3b06cb2a251ee0
guilhermeteixeira00 <guilherme.teixeira00@outlook.com>
2024-11-15 12:23:48 -0300

    Update Add contact api and fix colors

---
+++ js/contato.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form");
+const username = document.getElementById("nome");
+const email = document.getElementById("email");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const mensagemValue = mensagem.value;
+
+    const url = ('https://wa.me/+5561999647021/?text= ' + 
+        ' Nome: ' + usernameValue + '\n' + 
+        ' Email: ' + emailValue + '\n' +
+        ' Mensagem:' + mensagemValue);
+
+    const win = window.open(url, '_blank')
+    win.focus()
+})
