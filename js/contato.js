a8a39108ff58f5b24188acea6f8a5a1166cf09e2
alexandre1Work <alexandre.velosowork@gmail.com>
2024-11-18 11:37:12 -0300

    Depoimentos

--- js/contato.js
+++ js/contato.js
@@ -1,18 +1,18 @@
 const form = document.getElementById("form");
 const username = document.getElementById("nome");
-const email = document.getElementById("email");
+// const email = document.getElementById("email");
 const mensagem = document.getElementById("mensagem");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const usernameValue = username.value;
-    const emailValue = email.value;
+    // const emailValue = email.value;
     const mensagemValue = mensagem.value;
 
-    const url = ('https://wa.me/+5561999647021/?text= ' + 
+    const url = ('https://wa.me/+5538998815385/?text= ' + 
         ' Nome: ' + usernameValue + '\n' + 
-        ' Email: ' + emailValue + '\n' +
+        // ' Email: ' + emailValue + '\n' +
         ' Mensagem:' + mensagemValue);
 
     const win = window.open(url, '_blank')
